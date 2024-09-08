ded848334f1c68e16271cc44540952dc372778e1
AndreySVRF <andreysmagin14@gmail.com>
2024-09-08 13:07:22 +0400

    feature: Add db connect witch docker-compose.yml

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"kanban/pkg/config"
+	"kanban/pkg/db"
 )
 
 func main() {
+	// Считываем конфигурацию
 	cfg, err := config.LoadConfig("configs/config.yml")
 	if err != nil {
 		panic(fmt.Errorf("не удалось загрузить конфигурацию: %w", err))
 	}
 
-	fmt.Printf("Запуск сервера на порту: %s\n", cfg.AppPort)
-	fmt.Printf("Подключение к базе данных: %s:%s\n", cfg.Database.Host, cfg.Database.Port)
+	// Подключаемся к базе данных
+	dbpool, err := db.NewDBConnection(cfg)
+	if err != nil {
+		log.Fatalf("Ошибка подключения к базе данных: %v", err)
+	}
+	defer dbpool.Close()
+
+	fmt.Println("Успешное подключение к базе данных!")
 }
